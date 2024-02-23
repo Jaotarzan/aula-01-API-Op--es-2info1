76556458895f900a8fd5b65b416129b4ae313306
Jaotarzan <joaovssouza59@gmail.com>
2024-02-23 11:40:43 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+data() {
+  return {
+    contador: 0
+  }
+},
+methods: {
+  incrementar() {
+    this.contador++;
+  },
+  decrementar() {
+    if (this.contador > 0) this.contador--;
+  },
+  reiniciar() {
+    this.contador = 0
+  }
+}
+}
+
+</script>
+
+<template>
+  <button @click="incrementar">+</button>
+  <button @click="decrementar">-</button>
+  <button @click="reiniciar">0</button>
+  <p>valor do contador: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  color: blueviolet;
+  font-weight: bold;
+  background-color: black;
+  border: none;
+}
+</style>
